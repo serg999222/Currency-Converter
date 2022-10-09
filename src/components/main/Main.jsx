f01f218c16fbc9aa418a53db0288d79c4cbad9a0
serg999222 <gazenkos935@gmail.com>
2022-10-09 15:43:59 +0300

    add brackpoint

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -5,12 +5,15 @@ import GetArea from './GetArea'
 
 
 function Main(props) {
+	let date = new Date
+	
+
 	return (
 
 		<main className="main">
 			<div className='title-block'>
 				<div className='title-block__title'>Converter</div>
-				<div className='title-block__date'>Date: 08.10.2022</div>
+				<div className='title-block__date'>{date.getDate()}.{date.getMonth()+1}.{date.getFullYear()}</div>
 			</div>
 			<div className='exchange-block'>
 				<ChangeArea {...props}/>
